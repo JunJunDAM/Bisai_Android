d191f1b8d86f90010a7f17b7d3d39a758f1f904e
Sergi Tirado <sergi2496@hotmail.com>
2017-04-24 17:38:38 +0200

    Login ok

---
+++ app/src/main/java/com/bisai/bisai/controller/activities/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bisai.bisai.controller.activities.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bisai.bisai.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/bisai/bisai/util/CustomProperties.java
@@ -0,0 +1,9 @@
+package com.bisai.bisai.util;
+
+public class CustomProperties {
+    public static String clientId = "bisaiapp";
+    public static String clientSecret = "my-secret-token-to-change-in-production";
+    public static String grantType = "password";
+    public static String scope = "read write";
+    public static String baseUrl = "http://192.168.1.33:8080";
+}
